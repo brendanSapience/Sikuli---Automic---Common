768aa87a18066b9f652227aa641d3340893bfaa4
brend <brend@DESKTOP-FFPADSA>
2017-02-09 12:09:23 -0500

    correction

--- src/sikulitests/goStart.java
+++ src/sikulitests/goStart.java
@@ -4,6 +4,7 @@ import java.io.PrintStream;
 import org.sikuli.script.App;
 import org.sikuli.script.FindFailed;
 import org.sikuli.script.ImagePath;
+import org.sikuli.script.Key;
 import org.sikuli.script.Match;
 import org.sikuli.script.Screen;
 
@@ -14,27 +15,22 @@ public class goStart
   private static String IEXPLORERLABEL = "iexplorer";
   
   private static String IMAGEFOLDER = "images";
-  public static void main(String[] args)
-    throws InterruptedException
-  {
-    try
-    {
+  public static void main(String[] args) throws InterruptedException{
+
     	// the App object represents the instance of the App to open and interact with
       App objApp = null;
       
       String BROWSER = "firefox";
       
       // Handling multiple browsers in one
-      if (BROWSER.equalsIgnoreCase(CHROMELABEL))
-      {
+      if (BROWSER.equalsIgnoreCase(CHROMELABEL)){
         ImagePath.setBundlePath("./src/"+IMAGEFOLDER+"/chrome");
         // the following line is necessary when this code is compiled into a standalone jar. it allows the code to retrieve the images stored in the jar file.
         ImagePath.add("sikulitests.goStart/"+IMAGEFOLDER+"/chrome");
         
         objApp = App.open("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe");
       }
-      if (BROWSER.equalsIgnoreCase(FIREFOXLABEL))
-      {
+      if (BROWSER.equalsIgnoreCase(FIREFOXLABEL)){
         ImagePath.setBundlePath("./src/"+IMAGEFOLDER+"/firefox");
         ImagePath.add("sikulitests.goStart/"+IMAGEFOLDER+"/firefox");
         
@@ -43,40 +39,35 @@ public class goStart
       }
       
       // Wait a few seconds for the app to load
-      Thread.sleep(2000L);
+      Thread.sleep(2000);
       
       Screen objScreen = new Screen();
       
       objScreen.type("searchbar.png", "www.automic.com");
-      objScreen.type("\n");
+      objScreen.type(Key.ENTER);
       
-      Thread.sleep(1000L);
-      try
-      {
-        Match localMatch = objScreen.wait("marketplace.png", 10.0D);
+      Thread.sleep(1000);
+      try{
+        Match localMatch = objScreen.wait("marketplace.png", 10);
       }
-      catch (FindFailed e)
-      {
-        throw new Exception("Marketplace icon not found within 10 seconds");
+      catch (FindFailed e){
+        System.err.println("Marketplace Website did not load in time.");
+        System.exit(999);
       }
       objScreen.click();
-      try
-      {
-    	  Match localMatch = objScreen.wait("marketplace_login.png", 10.0D);
+      try{
+    	  Match localMatch = objScreen.wait("marketplace_login.png", 10);
       }
-      catch (FindFailed e)
-      {
-        throw new Exception("Marketplace login icon not found within 10 seconds");
+      catch (FindFailed e){
+          System.err.println("Marketplace Login did not load in time.");
+          System.exit(999);
       }
       objScreen.click();
       
       objScreen.type("marketplace_login_login.png", "brendan.sapience@automic.com");
-      objScreen.type("marketplace_login_password.png", "Delta55!");
+      objScreen.type("marketplace_login_password.png", "MyPassword123");
       objScreen.type("\n");
-    }
-    catch (Exception e)
-    {
-      System.out.println(e.toString());
-    }
+    
+
   }
 }
